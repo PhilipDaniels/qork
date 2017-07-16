e22ae96c7b82f371b31cfb91947b3ef6b0be773e
Philip Daniels <Philip.Daniels1971@gmail.com>
2017-07-16 14:51:31 +0100

    Add slog and slog-term and do basic logging.

---
+++ src/rust/qork/src/main.rs
@@ -0,0 +1,26 @@
+#[macro_use]
+extern crate slog;
+extern crate slog_term;
+extern crate slog_async;
+
+use slog::Drain;
+
+fn main() {
+	let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+	let log = slog::Logger::root(drain, o!());
+
+	trace!(log, "hello world");
+	debug!(log, "hello world");
+	info!(log, "hello world");
+	warn!(log, "hello world");
+	error!(log, "hello world");
+	crit!(log, "hello world");
+}
+
+/*
+ * fn main() {
+    println!("Hello, worlds!");
+}
+*/
