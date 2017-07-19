13e7165122121d8df7f66fbb3de0e5593f5f66a6
Philip Daniels <Philip.Daniels1971@gmail.com>
2017-07-19 20:49:56 +0100

    Tidy up.

--- src/rust/qork/src/main.rs
+++ src/rust/qork/src/main.rs
@@ -8,22 +8,24 @@ mod execution_timer;
 use execution_timer::ExecutionTimer;
 
 fn main() {
-	let logger = create_root_logger();
-	let _timer = ExecutionTimer::new(&logger, "Main.Start");
+    let logger = create_root_logger();
+    let _timer = ExecutionTimer::new(&logger, "Main.Start");
 }
 
 fn create_root_logger() -> Logger {
-	// trace, debug, info, warn, error, crit.
-	use slog::Drain;
+    // trace, debug, info, warn, error, crit.
+    use slog::Drain;
 
-	let decorator = slog_term::TermDecorator::new().build();
+    let decorator = slog_term::TermDecorator::new().build();
     let drain = slog_term::FullFormat::new(decorator).build().fuse();
     let drain = slog_async::Async::new(drain).build().fuse();
-	slog::Logger::root(drain, o!())
+
+    // TODO: We want dynamic configuration here.
+    slog::Logger::root(drain, o!())
 }
 
 fn sleep(msec: u64) {
-	use std::thread;
-	use std::time::Duration;
-	thread::sleep(Duration::from_millis(msec));
+    use std::thread;
+    use std::time::Duration;
+    thread::sleep(Duration::from_millis(msec));
 }
