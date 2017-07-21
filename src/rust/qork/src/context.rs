8df55974de16ac6edb957aa642d6c04390e6ae7d
Philip Daniels <Philip.Daniels1971@gmail.com>
2017-07-21 15:59:26 +0100

    Write a function to determine where .qork.d is.

--- src/rust/qork/src/context.rs
+++ src/rust/qork/src/context.rs
@@ -3,6 +3,8 @@ use slog::Logger;
 use std::time::{SystemTime, UNIX_EPOCH};
 use chrono::{Utc, TimeZone, DateTime};
 use hostname;
+use std::path::PathBuf;
+use std::env;
 
 // The complete execution context of Qork.
 pub struct Context {
@@ -10,7 +12,8 @@ pub struct Context {
     pub args: Vec<String>,
     pub exe_path: Option<std::path::PathBuf>,
     pub exe_meta_data: Option<std::fs::Metadata>,
-    pub hostname: Option<String>
+    pub hostname: Option<String>,
+    pub config_directory: PathBuf
 }
 
 fn system_time_to_date_time(t: SystemTime) -> DateTime<Utc> {
@@ -29,10 +32,48 @@ fn system_time_to_date_time(t: SystemTime) -> DateTime<Utc> {
     Utc.timestamp(sec, nsec)
 }
 
+fn get_config_directory(logger: &Logger) -> PathBuf {
+    const CONFIG_DIR_KEY : &'static str = "config_dir";
+    const CONFIG_DIR : &'static str = ".qork.d";
+
+    // Command line has highest priority.
+    // let msg = "Configuration Directory determined from command line arguments.";
+
+    // Next is an environment variable.
+    if let Ok(env_var) = std::env::var("QORK_CONFIG_DIR") {
+        if !env_var.is_empty() {
+            info!(logger, "Configuration Directory determined from QORK_CONFIG_DIR environment variable"; CONFIG_DIR_KEY => &env_var);
+            return PathBuf::from(env_var);
+        }
+    }
+
+    // If still no luck, try for '.qork.d' in the user's home directory - if we can
+    // determine the user's home directory, that is.
+    if let Some(mut home_dir) = env::home_dir() {
+        home_dir.push(CONFIG_DIR);
+        info!(logger, "Configuration Directory defaulted to '{}' in user's home directory", CONFIG_DIR; CONFIG_DIR_KEY => %&home_dir.display());
+        return home_dir;
+    }
+
+    // Still stuck? Try .qork.d in the exe's directroy.
+    if let Ok(mut exe_path) = std::env::current_exe() {
+        exe_path.pop();
+        exe_path.push(CONFIG_DIR);
+        info!(logger, "Configuration Directory defaulted to '{}' in directory of the exe", CONFIG_DIR; CONFIG_DIR_KEY => %&exe_path.display());
+        return exe_path;
+    }
+
+    // Last, just default to .qork.d in the current working directory.
+    warn!(logger, "Unable to determine Configuration Directory from command line parameter, home dir, or exe dir: defaulting to '{}' in the current working directory", CONFIG_DIR;
+        CONFIG_DIR_KEY => CONFIG_DIR);
+    PathBuf::from(CONFIG_DIR)
+}
+
 impl Context {
     pub fn new(logger: Logger) -> Context {
         let exe = std::env::current_exe().ok();
         let md = exe.as_ref().and_then(|e| e.metadata().ok());
+        let cd = get_config_directory(&logger);
 
         Context {
             logger: logger,
@@ -40,6 +81,7 @@ impl Context {
             exe_path: exe,
             exe_meta_data: md,
             hostname: hostname::get_hostname(),
+            config_directory: cd
         }
     }
 
@@ -63,6 +105,7 @@ impl Context {
             .unwrap_or("unknown".to_string());
 
         info!(self.logger, "Created Context";
+               "config_directory" => &self.config_directory.to_str(),
                "version" => self.version(),
                "hostname" => &self.hostname,
                "exe_modified" => mdate,
