d1a74417ee6c3f9a1aa6bacb9d225909be44304c
Philip Daniels <Philip.Daniels1971@gmail.com>
2017-07-24 22:50:46 +0100

    ADD(Config): Start on loading the user configuration directory.

--- src/rust/qork/src/main.rs
+++ src/rust/qork/src/main.rs
@@ -22,14 +22,15 @@ use slog::Logger;
 fn main() {
     std::env::set_var("IN_QORK", "1");
     let throw_away_logger = create_root_logger();
-    let _timer = ExecutionTimer::new(&throw_away_logger, "Main.Start");
-
+    let _timer = ExecutionTimer::new(&throw_away_logger, "main.main");
 
     let args = CommandLineArguments::new();
     let main_logger = create_root_logger();
     info!(&main_logger, "{:?}", args);
     let context = Context::new(main_logger, args);
     context.log_created_message();
+
+    load_user_configuration_if_valid(&context);
 }
 
 fn create_root_logger() -> Logger {
@@ -41,4 +42,28 @@ fn create_root_logger() -> Logger {
 
     // TODO: We want dynamic configuration here.
     slog::Logger::root(drain, o!())
+}
+
+fn load_user_configuration_if_valid(context: &Context) {
+    if context.command_line_arguments().load_config() {
+        let _timer = ExecutionTimer::new2(&context.logger(), "load_user_configuration");
+        let dir = context.xdg().get_config_home();
+        if !dir.exists() {
+            warn!(&context.logger(), "The config_directory does not exist, no config will be loaded"; "config_directory" => %dir.display());
+            return
+        }
+
+        if !dir.is_dir() {
+            warn!(&context.logger(), "The config_directory is a file, not a directory, no config will be loaded"; "config_directory" => %dir.display());
+            return
+        }
+
+        load_user_configuration(context);
+
+    } else {
+        info!(&context.logger(), "Loading of user configuration is disabled.");
+    }
+}
+
+fn load_user_configuration(context: &Context) {
 }
