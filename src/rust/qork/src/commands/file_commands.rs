129c54e767afd33bdd967a6727942a9fa7e0d8c0
Philip Daniels <Philip.Daniels1971@gmail.com>
2017-09-06 08:03:30 +0100

    Commands: call expand_variables from handle_open_file.
    Makes it possible to say "open ~/Documents/foo.txt". There is a question
    of where exactly in the call stack expand_variables should be called -
    early or late - but that is a question for later.

--- src/rust/qork/src/commands/file_commands.rs
+++ src/rust/qork/src/commands/file_commands.rs
@@ -1,8 +1,10 @@
 use std::path::Path;
 use context::Context;
 use buffer::Buffer;
+use utils;
 
 pub fn handle_open_file(context: &Context, filename: String) {
+    let filename = utils::expand_variables(&filename).to_string();
     match Buffer::open_file(Path::new(&filename)) {
         Some(buf) => context.state().mru().insert(filename),
         None => {}
